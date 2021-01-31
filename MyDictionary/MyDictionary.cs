fb3774b8683d9c4e1e2c02bf6f542eed33ba7da3
Enis Emir <enisemirr@gmail.com>
2021-01-31 03:07:22 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<TKey, TValue>
+    {
+        TKey[] arrayKeys;
+        TKey[] tempKeys;
+
+        TValue[] arrayValues;
+        TValue[] tempValues;
+
+        public MyDictionary()
+        {
+            arrayKeys = new TKey[0];
+            arrayValues = new TValue[0];
+
+        }
+        public void Add(TKey key, TValue value)
+        {
+            tempKeys = arrayKeys;
+            tempValues = arrayValues;
+            arrayKeys = new TKey[arrayKeys.Length+1];
+            arrayValues = new TValue[arrayValues.Length + 1];
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                arrayKeys[i] = tempKeys[i];                
+            }
+            for (int i = 0; i < tempValues.Length; i++)
+            {
+                arrayValues[i] = tempValues[i];
+            }
+
+            arrayKeys[arrayKeys.Length - 1] = key;
+            arrayValues[arrayValues.Length - 1] = value;
+
+            Console.WriteLine("ID :" + arrayKeys[arrayKeys.Length - 1] + "    Name :" + arrayValues[arrayValues.Length - 1] + "   listeye eklenmiştir!!!!!");
+            Console.WriteLine("");
+
+            for (int i = 0; i < arrayValues.Length; i++)
+            {
+
+                Console.WriteLine("ID :" + arrayKeys[i] + "    Name :" + arrayValues[i]);
+            }
+            Console.WriteLine("");
+            Console.WriteLine("");
+        }
+        
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> ogrenci = new MyDictionary<int, string>();
+
+            ogrenci.Add(1, "Enis");
+            ogrenci.Add(1, "Okan");
+            ogrenci.Add(1, "Muzaffer");
+            ogrenci.Add(1, "Ahmet");
+            ogrenci.Add(1, "Serdar");
+            ogrenci.Add(1, "Fatih");
+            ogrenci.Add(1, "Ersin");
+        }
+    }
+}
